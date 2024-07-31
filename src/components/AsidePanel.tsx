60184311594eefc27e5df0f60995c87f3b89fbe9
Armin <armin2003sheibak@gmail.com>
2024-07-31 20:50:24 +0330

    build aside panel

---
+++ src/components/AsidePanel.tsx
@@ -0,0 +1,24 @@
+import { Drawer, DrawerOverlay, DrawerContent, DrawerHeader, DrawerBody, useDisclosure, Image } from '@chakra-ui/react'
+import navicon from '../assets/navicon.webp'
+
+const AsidePanel = () => {
+  const {isOpen, onClose, onOpen} = useDisclosure()
+  return (
+    <>
+    <Image  src={navicon} boxSize='60px' objectFit='cover'  onClick={onOpen} />
+    <Drawer placement='right' isOpen={isOpen} onClose={onClose} > 
+     <DrawerOverlay />
+         <DrawerContent>
+           <DrawerHeader borderBottomWidth='1px'>Movies</DrawerHeader>
+           <DrawerBody>
+             <p>Some contents...</p>
+             <p>Some contents...</p>
+             <p>Some contents...</p>
+           </DrawerBody>
+         </DrawerContent>
+     </Drawer>
+     </>
+  )
+}
+
+export default AsidePanel
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,17 @@
-import { HStack, Image, Heading, Hide, Box } from "@chakra-ui/react"
+import { HStack, Image, Heading, Hide} from "@chakra-ui/react"
 import logo from '../assets/logo.webp'
+import AsidePanel from "./AsidePanel"
 
 const NavBar = () => {
   return (
-    <HStack paddingX='20px'  alignItems='center' >
-      <Image src={logo} boxSize='60px' objectFit='cover' />
-      <Hide below='md' >
-        <Box paddingBottom='7px'>
-          <Heading as='h1' color='whitesmoke' >Movie Center</Heading>
-        </Box>
-      </Hide>
+    <HStack paddingX='20px' paddingTop='10px' justifyContent='space-between'  alignItems='center' >
+      <HStack>
+        <Image src={logo} boxSize='60px' objectFit='cover' />
+        <Hide below='md' >
+            <Heading display='inline-block' as='h1' color='whitesmoke' paddingBottom='7px' >Movie Center</Heading>
+        </Hide>
+      </HStack>
+      <AsidePanel />
     </HStack>
   )
 }
