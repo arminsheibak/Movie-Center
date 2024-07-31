86a0a7df752e2616cdfdbaaef438aaf81e44dade
Armin <armin2003sheibak@gmail.com>
2024-07-31 11:20:13 +0330

    added a custom hook to fetch the movies

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,27 +1,9 @@
-import { useEffect, useState } from 'react'
-import apiClient from '../services/api-client';
+import useMovies from "../hooks/useMovies"
 
-interface Movie {
-  id: number;
-  title: string;
-
-}
-
-interface FetchMoviesResponse {
-  page: number;
-  results : Movie[];
-}
 
 const MovieGrid = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    apiClient.get<FetchMoviesResponse>('popular')
-    .then(res => setMovies(res.data.results))
-    .catch(err => setError(err.message))
-  })
+  const {movies, error} = useMovies()
+  
   return (
     <>
     {error && <p>{error}</p> }
---
+++ src/hooks/useMovies.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Movie {
+  id: number;
+  title: string;
+
+}
+
+interface FetchMoviesResponse {
+  page: number;
+  results : Movie[];
+}
+
+
+const useMovies = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    apiClient.get<FetchMoviesResponse>('popular')
+    .then(res => setMovies(res.data.results))
+    .catch(err => setError(err.message))
+    
+  }, []);
+
+  return {movies, error, isLoading}
+}
+
+export default useMovies;
