f5a4f766c62dc8b68ced8a609e8fc4082e91f243
Armin <armin2003sheibak@gmail.com>
2024-08-03 10:13:42 +0330

    handle errors

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,23 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 const ErrorPage = () => {
-  return (
-    <div>ErrorPage</div>
-  )
-}
+  const error = useRouteError();
 
-export default ErrorPage
+  return (
+    <>
+      <NavBar />
+      <Box padding={5} >
+        <Heading>Error</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "404 Page Not Found"
+            : "Unexpected Error"}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,13 +3,14 @@ import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import DiscoverMovies from "./pages/DiscoverMovies";
 import MovieDetailPage from "./pages/MovieDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {path: '/', element: <Layout />, children: [
     {index: true, element: <HomePage />},
     {path: 'movies', element: <DiscoverMovies />},
     {path: 'movies/:id', element: <MovieDetailPage />}
-  ]}
+  ], errorElement: <ErrorPage />}
 ])
 
 export default router;
